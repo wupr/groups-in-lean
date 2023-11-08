6facba763e7712f98575d2e5ec89ec5fc6918273
Peiran Wu <15968905+wupr@users.noreply.github.com>
2023-11-08 17:13:37 +0000

    Add lemma `comm_of_exponent_two`

--- GroupsInLean/Basic.lean
+++ GroupsInLean/Basic.lean
@@ -1,1 +1,10 @@
-def hello := "world"
+import Mathlib.Algebra.Group.Basic
+import Mathlib.Tactic
+import Paperproof
+
+variable (G : Type) [Group G]
+
+/-- If the square of every element of a group `G` is the identity, then `G` is abelian. -/
+lemma comm_of_exponent_two (h : ∀ x : G, x * x = 1) : ∀ x y : G, x * y = y * x := by
+  intro x y
+  rw [← mul_left_inj (x * y), h (x * y), mul_assoc, ← mul_assoc x x y, h x, one_mul, h y]
